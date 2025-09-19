f60771c04e14a4ddd91eb16185bc60af48c355bc
Srinivas Valluri <svalluri@bondstreetloans.com>
2025-09-19 13:53:10 +0530

    Random Color Generator[2]

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Accordian from './components/accordian';
+import RandomColor from './components/random-color';
 
 function App() {
   return (
     <div className="App">
       {/* Accordian Component */}
-      <Accordian />
+      {/* <Accordian /> */}
+
+      {/* Random Color Component */}
+      <RandomColor />
+
     </div>
   );
 }
